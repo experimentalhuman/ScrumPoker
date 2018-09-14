caac9da674a6d420c6350b6d226f3023fc79614b
mboyce <mboyce@nexient.com>
2018-09-14 14:24:48 -0400

    changed Deck class constructor

--- app/src/main/java/com/nexient/android/scrumpoker/Deck.java
+++ app/src/main/java/com/nexient/android/scrumpoker/Deck.java
@@ -1,8 +1,11 @@
 package com.nexient.android.scrumpoker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -15,12 +18,20 @@ public class Deck {
     private View backOfCard;
     private TextView frontOfCard;
 
-    public Deck(View multi_card, View card_hide, TextView card_show){
+    //animations for transitions
+    private final Animation animOut;
+    private final Animation animIn;
+
+    public Deck(View multi_card, View card_hide, TextView card_show, Context context){
         cards = multi_card;
         backOfCard = card_hide;
         frontOfCard = card_show;
+        animIn = AnimationUtils.loadAnimation(context, R.anim.flip_in);
+        animOut = AnimationUtils.loadAnimation(context, R.anim.flip_out);
     }
-    
+
+
+
     //to be called with the value of the button that's pressed
     //switches from the whole table view to the back of the card.
     public void chooseCard(String value){
@@ -28,28 +39,46 @@ public class Deck {
         frontOfCard.setText(value);
 
         //the back of the card pops up and others are made invisible
-        //TODO: animate
-        cards.setVisibility(View.GONE);
-        frontOfCard.setVisibility(View.GONE);
-        backOfCard.setVisibility(View.VISIBLE);
+        cards.startAnimation(animOut);
+        cards.postDelayed(new Runnable(){
+            public void run(){
+                cards.setVisibility(View.INVISIBLE);
+                backOfCard.setVisibility(View.VISIBLE);
+                backOfCard.startAnimation(animIn);
+            }
+        }, 500);
+        frontOfCard.setVisibility(View.INVISIBLE);
+
     }
 
     //to be called when the back of the card is touched to reveal
     public void reveal(){
         //switch so front of the card is the only thing visible
-        //TODO: animate
-        cards.setVisibility(View.GONE);
-        backOfCard.setVisibility(View.GONE);
-        frontOfCard.setVisibility(View.VISIBLE);
+        cards.setVisibility(View.INVISIBLE);
+
+        backOfCard.startAnimation(animOut);
+        backOfCard.postDelayed(new Runnable(){
+            public void run(){
+                backOfCard.setVisibility(View.INVISIBLE);
+                frontOfCard.setVisibility(View.VISIBLE);
+                frontOfCard.startAnimation(animIn);
+            }
+        }, 500);
     }
 
     //to be called when going back to the full table view
     public void discard(){
         //switch so only the table view is visible
-        //TODO: animate
-        backOfCard.setVisibility(View.GONE);
-        frontOfCard.setVisibility(View.GONE);
-        cards.setVisibility(View.VISIBLE);
+        backOfCard.setVisibility(View.INVISIBLE);
+
+        frontOfCard.startAnimation(animOut);
+        frontOfCard.postDelayed(new Runnable(){
+            public void run(){
+                frontOfCard.setVisibility(View.INVISIBLE);
+                cards.setVisibility(View.VISIBLE);
+                cards.startAnimation(animIn);
+            }
+        }, 500);
     }
 
 }
